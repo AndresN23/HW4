f2263b2a18b87a42753cdab03aed301e6d0527b2
Andres Naranjo <anblue62@gmail.com>
2026-04-05 12:23:05 -0700

    V3.1

--- index.mjs
+++ index.mjs
@@ -5,6 +5,7 @@ import getRandomQuote from 'get-random-quote';
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', async (req, res) => {
     try {
